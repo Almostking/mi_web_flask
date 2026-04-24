413f45099b38f11fa5f01d37eb4757a1df467b8b
Maximiliano <maximiliano.turresz@gmail.com>
2026-04-23 23:34:37 -0400

    Mejora: formulario funcional + diseño verde centrado

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def inicio():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/contacto', methods=['POST'])
+def contacto():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    mensaje = request.form['mensaje']
+
+    print(f"Nombre: {nombre}")
+    print(f"Email: {email}")
+    print(f"Mensaje: {mensaje}")
+
+    return redirect('/')
